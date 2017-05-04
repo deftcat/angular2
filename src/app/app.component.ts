7600da9d428ab954c636e86ad69175c342539a07
Fei <ycshao@nlg.csie.ntu.edu.tw>
2017-05-04 17:44:00 +0800

    split data as mock and initial it in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import {HeroService} from './hero.service';
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
   { id: 12, name: 'Narco' },
@@ -74,13 +75,22 @@ const HEROES: Hero[] = [
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+  providers : [HeroService],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  ngOnInit(){
+  	this.getHeroes();
+  }
+  // heroes = HEROES;
+  constructor(private heroService : HeroService){};
+  heroes : Hero[];
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+  getHeroes():void{
+  	this.heroes = this.heroService.getHeroes();
   }
 }
