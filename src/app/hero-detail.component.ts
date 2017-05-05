297c22c0f2c52d8906adc3d4e7ca1584ae1cc2fc
Fei <ycshao@nlg.csie.ntu.edu.tw>
2017-05-05 17:23:51 +0800

    add event and router

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,18 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import {HeroService} from './hero.service';
+import {ActivatedRoute, Params} from '@angular/router';
+import {Location} from '@angular/common';
+import 'rxjs/add/operator/switchMap';
+
 @Component({
   selector: 'hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-      </div>
-    </div>
-  `
+  templateUrl:'./hero-detail.component.html',
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit{
+
   @Input() hero: Hero;
+  ngOnInit(){
+    // this.route.params.
+    // switchMap((params : Params)=>this.heroService.getHeroes(+params['id']))
+    // .subscribe(hero =>{ this.hero = hero});
+      this.route.params
+    .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+    .subscribe(hero => this.hero = hero);
+  };
+  constructor(
+    private heroService : HeroService,
+    private location : Location,
+    private route : ActivatedRoute,
+    ){};
+    goBack():void{
+    this.location.back();
+  }
 }
