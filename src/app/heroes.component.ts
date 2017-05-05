37c3808c5348c98fe05f111e10d8db71c36d05f9
Fei <ycshao@nlg.csie.ntu.edu.tw>
2017-05-05 17:26:29 +0800

    add routing with id, and link to cssStyle

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import {HeroService} from './hero.service';
+import {Router} from '@angular/router'
  
 @Component({
   selector: 'my-heroes',
@@ -14,57 +15,12 @@ import {HeroService} from './hero.service';
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <hero-detail [hero]="selectedHero"></hero-detail>
+    <div *ngIf='selectedHero'>
+    	<h2>{{selectedHero.name}}</h2>
+		<button (click)='gotoDetail()'>See detail</button>
+    </div>
   `,
-  styles: [`
-    .selected {
-      background-color: #CFD8DC !important;
-      color: white;
-    }
-    .heroes {
-      margin: 0 0 2em 0;
-      list-style-type: none;
-      padding: 0;
-      width: 15em;
-    }
-    .heroes li {
-      cursor: pointer;
-      position: relative;
-      left: 0;
-      background-color: #EEE;
-      margin: .5em;
-      padding: .3em 0;
-      height: 1.6em;
-      border-radius: 4px;
-    }
-    .heroes li.selected:hover {
-      background-color: #BBD8DC !important;
-      color: white;
-    }
-    .heroes li:hover {
-      color: #607D8B;
-      background-color: #DDD;
-      left: .1em;
-    }
-    .heroes .text {
-      position: relative;
-      top: -3px;
-    }
-    .heroes .badge {
-      display: inline-block;
-      font-size: small;
-      color: white;
-      padding: 0.8em 0.7em 0 0.7em;
-      background-color: #607D8B;
-      line-height: 1em;
-      position: relative;
-      left: -1px;
-      top: -4px;
-      height: 1.8em;
-      margin-right: .8em;
-      border-radius: 4px 0 0 4px;
-    }
-  `],
+  styleUrls:['heroes.component.css'] ,
   // providers : [HeroService],
 })
 export class HeroesComponent implements OnInit{
@@ -74,7 +30,8 @@ export class HeroesComponent implements OnInit{
   	this.getHeroes();
   }
   // heroes = HEROES;
-  constructor(private heroService : HeroService){};
+  constructor(private heroService : HeroService
+  	, private router : Router){};
   heroes : Hero[];
   selectedHero: Hero;
   onSelect(hero: Hero): void {
@@ -84,5 +41,8 @@ export class HeroesComponent implements OnInit{
   	  	
   	// this.heroes = this.heroService.getHeroes();
   	this.heroService.getHeroes().then(heroes=>{this.heroes = heroes; console.log('trigge');});
+  }
+  gotoDetail():void{
+  	this.router.navigate(['./detail', this.selectedHero.id]);
   }
 }
