36fe36aff0bda8bf1d98b2270590a50defda11ab
Fei <ycshao@nlg.csie.ntu.edu.tw>
2017-05-08 10:04:02 +0800

    extract html file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,7 @@ import {Component} from '@angular/core';
 // import {HeroService} from './hero.service';
 @Component ({
 	selector : 'app-root',
-	template:`<h1>{{title}}</h1>
-	<nav>
-		<a routerLink='/heroes'>Heroes</a>
-	<a routerLink='/dashboard'>DashBoard</a>
-	</nav>
-	<router-outlet></router-outlet>`,
+	templateUrl:'app.component.html',
 	styleUrls : ['./app.component.css']
 
 })
